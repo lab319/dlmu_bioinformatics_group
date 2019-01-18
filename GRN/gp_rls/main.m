f1b18ad67a609be107bfc43d4b843c98bc6b2f5c
lab319 <39893937+lab319@users.noreply.github.com>
2019-01-18 18:42:52 +0800

    Update main.m

--- GRN/gp_rls/main.m
+++ GRN/gp_rls/main.m
@@ -10,6 +10,7 @@
 
 clear 
 load e_cell
+iteration=20;
 n=53;
 X(:,1:3)=x(1:n,1:3);
 Y=x(1:n,4); 
@@ -43,7 +44,7 @@ disp(gprls_result([],0));
 disp(gprls_result(popu,1));
 
 % GP+rls loops
- for c = 2:20,
+ for c = 2:iteration
   %iterate 
   popu = gprls_mainloop(popu,X,Y,[],opt);
   disp(gprls_result(popu,1));
